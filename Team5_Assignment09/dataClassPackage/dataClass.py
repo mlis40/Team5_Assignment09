845f9572a7f6e1aae8476d247b79ccbf05645429
mliso <mliso@DESKTOP-V75G00O.wifi.uc.edu>
2024-03-28 08:59:01 -0400

    Adding documentation and website url

---
+++ Team5_Assignment09/dataClassPackage/dataClass.py
@@ -0,0 +1,12 @@
+# dataClass.py
+# Name: Matthew Lisowsky, Harrison Moore, Maddy Wogomon
+# email: lisowsmd@mail.uc.edu, wogomomr@mail.uc.edu, moorehc@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/04/2024
+# Course/Section: IS4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+# Citations:
+# Anything else that's relevant:
+
+# imports (ex: from functionPackage.functions import *)
---
+++ Team5_Assignment09/mainPackage/main.py
@@ -0,0 +1,18 @@
+# main.py
+# Name: Matthew Lisowsky, Harrison Moore, Maddy Wogomon
+# email: lisowsmd@mail.uc.edu, wogomomr@mail.uc.edu, moorehc@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/04/2024
+# Course/Section: IS4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+# Citations:
+# Anything else that's relevant:
+
+# imports (ex: from functionPackage.functions import *)
+
+
+if __name__ == "__main__": 
+    '''
+    https://pokeapi.co/api/v2/pokemon-species/appletun
+    '''
