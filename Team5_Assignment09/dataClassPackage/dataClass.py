bce4f9d0bc92872a3b3a27aced45aee1b732ee9c
mliso <mliso@DESKTOP-V75G00O.wifi.uc.edu>
2024-04-03 11:05:54 -0400

    Added the iteration function and some code to the main

--- Team5_Assignment09/dataClassPackage/dataClass.py
+++ Team5_Assignment09/dataClassPackage/dataClass.py
@@ -10,3 +10,16 @@
 # Anything else that's relevant:
 
 # imports (ex: from functionPackage.functions import *)
+
+# Iterating a dictionary function
+def iterate_dictionary(myDictionary):
+    for k, v in myDictionary.items():
+        print ("key is " + str(type(k)) + ", value is " + str(type(v)))
+        if isinstance(v, dict):
+            iterate_dictionary(v)
+        else:
+            print("{0} : {1}".format(k, v))
+            if (isinstance(v, list)):
+                for vv in v:
+                    if (isinstance(vv, dict)):
+                        iterate_dictionary(vv)
--- Team5_Assignment09/mainPackage/main.py
+++ Team5_Assignment09/mainPackage/main.py
@@ -10,9 +10,31 @@
 # Anything else that's relevant:
 
 # imports (ex: from functionPackage.functions import *)
-
+import json
+import requests
+'''
+import dataClassPackage.dataClass import iterate_dictionary
+'''
 
 if __name__ == "__main__": 
     '''
-    https://pokeapi.co/api/v2/pokemon-species/appletun
+    I added the API string to the correct location just make sure thats the right one
+    I also added the iterate function in the dataClass package
+    We are going to have to change the info that we want to pull which should go where the total and for statement is at
+    Additionally we will need to use the iterate function then we can use that import statement from above
+    Harrison can you look at the API like and write a comment above that u worked on the API stuff
+    Maddy can you try an work out what info we want to pull from the API
     '''
+    response = requests.get('https://pokeapi.co/api/v2/pokemon-species/appletun')
+    json_string = response.content
+    
+    parsed_json = json.loads(json_string) # Now we have a python dictionary
+    
+    #print(parsed_json)
+    #print(parsed_json['data'][0]['description'])
+    #print(parsed_json['data'][0]['directionsInfo'])
+    
+    total = int(parsed_json['total']) # The number of parks that were returned
+    
+    for park in parsed_json['data']:
+        print (park)
