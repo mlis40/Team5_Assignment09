249be5dfdeac8a30a6917554b13197c21caa94d4
mliso <mliso@DESKTOP-V75G00O.lan>
2024-04-03 21:23:55 -0400

    Final Commit, small code edits

--- Team5_Assignment09/mainPackage/main.py
+++ Team5_Assignment09/mainPackage/main.py
@@ -5,30 +5,25 @@
 # Due Date: 4/04/2024
 # Course/Section: IS4010-002
 # Semester/Year: Spring 2024
-# Brief Description of the assignment: This is a program that uses a Pokemon API to pull information from a massive JSON database about Pokemon. We chose to pull info on Maddy's favorite Appletun.
+# Brief Description of the assignment: This is a program that uses a Pokemon API to pull information from a massive JSON database
+#                                      about Pokemon. We chose to pull info on Maddy's favorite Appletun.
 # Citations: https://pokeapi.co/
 # Anything else that's relevant: 
 
 # imports (ex: from functionPackage.functions import *)
 import json
 import requests
-'''
-import dataClassPackage.dataClass import iterate_dictionary
-'''
 
+# Matt worked on the GitHub and architecure of the code
 if __name__ == "__main__": # Entry point
   
-    #Harrison found the API and set up to pull information on Appletun
+    # Harrison found the API and set up to pull information on Appletun
     response = requests.get('https://pokeapi.co/api/v2/pokemon-species/appletun')
     json_string = response.content
     
     parsed_json = json.loads(json_string) # Now we have a python dictionary
     
-    #print(parsed_json)
-    #print(parsed_json['data'][0]['description'])
-    #print(parsed_json['data'][0]['directionsInfo'])
-    
-    #Maddy worked on what info would pulled by the API below
+    # Maddy worked on what info would pulled by the API below
     
     pokemon_name = parsed_json['name'] #Pull and label the Pokemon name
     print(f"Name: {pokemon_name}")
